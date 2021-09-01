52aa0f4b981cb210929ccdcc3b8871f62f518be4
Serena Zhou <mediocrephilosophie@gmail.com>
2021-08-31 17:03:28 -0700

    s

--- act3/jailscenes/jail5.gd
+++ act3/jailscenes/jail5.gd
@@ -5,9 +5,9 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+func _ready():
+	$dialoguebox2.hide()
 
 
-
-#func _on_dialoguebox_tree_exited():
-	#Transition.change_scene("res://act3/scene2/hallway.tscn")
-
+func _on_dialoguebox_tree_exited():
+	$dialoguebox2.show()
