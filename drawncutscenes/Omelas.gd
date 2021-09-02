d10fc11ca132a9a05edc37453e554c84f7f02af3
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-01 21:11:06 -0700

    A

---
+++ drawncutscenes/Omelas.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var dialogue = [
+	"IT WAS ONE OF THE FEW SURVIVING TEXTS FROM BEFORE THE FIRST CATACLYSM.",
+	"",
+]
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite9.hide()
+	$Sprite2.hide()
+	$Sprite3.hide()
+	$Sprite4.hide()
+	$Sprite5.hide()
+	$Sprite6.hide()
+	$Sprite7.hide()
+	$Sprite8.hide()
+	
+func _process(delta):
+	$
