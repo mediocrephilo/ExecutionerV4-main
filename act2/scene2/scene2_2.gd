50f3ed2a9d7ddb134fb4f338a4b69925418149c1
inchworm <ebob123111@gmail.com>
2021-09-02 13:43:30 -0700

    awserdtfyugihojkpl';'

--- act2/scene2/scene2_2.gd
+++ act2/scene2/scene2_2.gd
@@ -11,6 +11,7 @@ func _ready():
 	$AudioStreamPlayer.play()
 	$AudioStreamPlayer2.play()
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
