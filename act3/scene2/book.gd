6ca6a522a60a906b3eac69e9e2d0e22d72e3e005
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-05 11:42:25 -0700

    m

--- act3/scene2/book.gd
+++ act3/scene2/book.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 var dialogue = [
-	"HMM . . . 'BEFORE THE CATACLYSMS: CIVILIZATION AND DIPLOMACY' BY TIMOTHIE SATO. SOUNDS . . . ENGAGING.",
+	"HMM . . . 'BEFORE THE CATACLYSMS: CIVILIZATION AND DIPLOMACY' BY TIMOTHIE SATO. SOUNDS . . . RIVETING.",
 ]
 var canInteract = false
 var dialogue_index = 0
@@ -14,6 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$"dialoguebox/continuesprite".show() == finished
 	if canInteract == true and Input.is_action_just_pressed("ui_accept"):
 		if dialogue_index == 0:
 			load_dialogue()
@@ -21,18 +22,18 @@ func _process(delta):
 			Transition.change_scene("res://act3/scene2/book1.tscn")
 		
 func load_dialogue():
-	$TextureRect.show()
-	$RichTextLabel.show()
+	$dialoguebox/TextureRect.show()
+	$dialoguebox/RichTextLabel.show()
 	if dialogue_index < dialogue.size():
 		finished = false
-		$Type.play()
-		$RichTextLabel.bbcode_text = dialogue[dialogue_index]
-		$RichTextLabel.percent_visible = 0
-		$Tween.interpolate_property(
-			$RichTextLabel, "percent_visible", 0, 1, 2, 
+		$dialoguebox/Type.play()
+		$dialoguebox/RichTextLabel.bbcode_text = dialogue[dialogue_index]
+		$dialoguebox/RichTextLabel.percent_visible = 0
+		$dialoguebox/Tween.interpolate_property(
+			$dialoguebox/RichTextLabel, "percent_visible", 0, 1, 2, 
 			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT
 		)
-		$Tween.start()
+		$dialoguebox/Tween.start()
 	else:
 		queue_free()
 
@@ -50,5 +51,5 @@ func _on_book_body_exited(body):
 
 func _on_Tween_tween_completed(_object, _key):
 	finished = true
-	$Type.stop()
+	$dialoguebox/Type.stop()
 	dialogue_index += 1
