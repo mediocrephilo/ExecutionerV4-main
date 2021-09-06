645660177edcd93538991cfcb6c5cb4245e81bed
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-06 15:41:05 -0700

    s

--- act3/scene2/poem.gd
+++ act3/scene2/poem.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var dialogue = [
+	"",
+]
 
 
 # Called when the node enters the scene tree for the first time.
