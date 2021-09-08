5bee4d5a34072de9510b1c3130714040700c9250
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-08 12:29:35 -0700

    s
    x

--- act3/scene2/poem.gd
+++ act3/scene2/poem.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
-
+var page = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$Martius.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		if page == 0:
+			$AudioStreamPlayer.play()
+			$Eudaimonia.hide()
+			$Martius.show()
+			yield(get_tree().create_timer(0.5), "timeout")
+			page = 1
+			
+	if Input.is_action_just_pressed("ui_left"):
+		if page == 1:
+			$AudioStreamPlayer.play()
+			$Eudaimonia.show()
+			$Martius.hide()
+			yield(get_tree().create_timer(0.5), "timeout")
+			page = 0
