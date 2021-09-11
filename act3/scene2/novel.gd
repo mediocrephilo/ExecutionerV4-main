a4bc36390d1ff5fade91e4373146b7678afda6ef
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-10 21:14:51 -0700

    s

--- act3/scene2/novel.gd
+++ act3/scene2/novel.gd
@@ -6,11 +6,14 @@ extends Node2D
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Label.hide()
+	yield(get_tree().create_timer(5), "timeout")
+	$Label.show()
+	$Label/AnimationPlayer.play("New Anim")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _process(delta):
+	if Input.is_action_just_pressed("escape"):
+		Transition.change_scene("res://act3/scene2/library")
