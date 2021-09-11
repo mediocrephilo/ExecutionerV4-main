5a31701159ef0a723dffd83dca6d69025d0d0211
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-10 21:13:17 -0700

    s

--- act3/scene2/textbook.gd
+++ act3/scene2/textbook.gd
@@ -8,9 +8,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Label.hide()
+	yield(get_tree().create_timer(5), "timeout")
+	$Label.show()
+	$Label/AnimationPlayer.play("New Anim")
 
 
 
 func _process(delta):
-
+	if Input.is_action_just_pressed("escape"):
+		Transition.change_scene("res://act3/scene2/library")
