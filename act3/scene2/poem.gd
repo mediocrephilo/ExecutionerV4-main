02ba5255665a8b095d0f27be212953e09667b9a3
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-13 08:51:49 -0700

    d

--- act3/scene2/poem.gd
+++ act3/scene2/poem.gd
@@ -5,6 +5,10 @@ var page = 0
 func _ready():
 	$Label.hide()
 	$Martius.hide()
+	$AnimatedSprite.hide()
+	$AnimatedSprite2.hide()
+	yield(get_tree().create_timer(3), "timeout")
+	$AnimatedSprite.show()
 	yield(get_tree().create_timer(10), "timeout")
 	$Label.show()
 	$Label/AnimationPlayer.play("New Anim")
@@ -12,18 +16,25 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("ui_right"):
 		if page == 0:
+			$AnimatedSprite.hide()
 			$AudioStreamPlayer.play()
 			$Eudaimonia.hide()
 			$Martius.show()
 			yield(get_tree().create_timer(0.5), "timeout")
 			page = 1
+			yield(get_tree().create_timer(3), "timeout")
+			$AnimatedSprite2.show()
 			
 	if Input.is_action_just_pressed("ui_left"):
 		if page == 1:
+			$AnimatedSprite2.hide()
 			$AudioStreamPlayer.play()
 			$Eudaimonia.show()
 			$Martius.hide()
 			yield(get_tree().create_timer(0.5), "timeout")
 			page = 0
+			yield(get_tree().create_timer(3), "timeout")
+			$AnimatedSprite.show()
+			
 	if Input.is_action_just_pressed("ui_cancel"):
 		Transition.change_scene("res://act3/scene2/library1")
