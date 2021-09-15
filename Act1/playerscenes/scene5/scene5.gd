7842af46f899b2c08b2c1b07e57f295383b06238
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-14 20:25:41 -0700

    yay game over done

--- Act1/playerscenes/scene5/scene5.gd
+++ Act1/playerscenes/scene5/scene5.gd
@@ -14,3 +14,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "player":
+		GameOver.transition("res://Act1/playerscenes/scene5/scene5.tscn")
+		GameOver.change(true)
+		Transition.change_scene("res://global/gameover/gameOver.tscn")
