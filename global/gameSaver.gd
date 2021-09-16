37256a95256c11059cf4f4c6e664f28fa84a66e8
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-16 13:46:42 -0700

    s

--- global/gameSaver.gd
+++ global/gameSaver.gd
@@ -4,6 +4,7 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+const SaveGame = preload('res://global/saveGame.gd')
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
