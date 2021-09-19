8bccff565836d14f81e1a35dc1b250ae4db52447
inchworm <ebob123111@gmail.com>
2021-09-18 18:48:52 -0700

    so much dying to do

--- act3/scene2/Sa.gd
+++ act3/scene2/Sa.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 
+
 func _on_Area2D_body_entered(body):
-	if body.name == "player2":
-		GameOver.transition("res://act3/scene5/obstaclecouse.tscn")
+	if body.name == "player":
+		GameOver.transition("res://act3/scene2/hallway.tscn")
 		GameOver.change(true)
 		Transition.change_scene("res://global/gameover/gameOver.tscn")
