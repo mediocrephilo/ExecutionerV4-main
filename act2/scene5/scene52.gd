488f7f0c772691dd3c674cb1b2fc7f409eee5d27
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-23 13:39:34 -0700

    s

--- act2/scene5/scene52.gd
+++ act2/scene5/scene52.gd
@@ -9,8 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SewerSound.playsound()
-	gameSaver.save_game("res://act2/scene5/scene5.tscn")
-
+	gameSaver.save_game("res://act2/scene5/scene52.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- act2/scene5/scene5after.gd
+++ act2/scene5/scene5after.gd
@@ -9,7 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SewerSound.playsound()
-	gameSaver.save_game("res://act2/scene5/scene5.tscn")
+	gameSaver.save_game("res://act2/scene5/scene5after.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
