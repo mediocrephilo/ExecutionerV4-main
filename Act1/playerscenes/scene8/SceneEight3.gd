9dc5406970d7fda611e076277757a1398e4d8ba6
Serena Zhou <mediocrephilosophie@gmail.com>
2021-09-25 12:56:31 -0700

    s

--- Act1/playerscenes/scene8/SceneEight3.gd
+++ Act1/playerscenes/scene8/SceneEight3.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gameSaver.save_game("res://Act1/playerscenes/scene8/SceneEight.tscn")
+	gameSaver.save_game("res://Act1/playerscenes/scene8/SceneEight3.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- Act1/playerscenes/scene8/letter1.gd
+++ Act1/playerscenes/scene8/letter1.gd
@@ -12,5 +12,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Transition.change_scene("res://Act1/scene8/SceneEight3")
 #	pass
